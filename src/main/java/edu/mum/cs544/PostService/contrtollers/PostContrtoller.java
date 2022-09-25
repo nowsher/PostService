9ff29803e9f7f5062002ae10fd81c27f3c3dddfd
nowsher <alinowsher@yahoo.com>
2022-09-25 13:57:58 -0500

    v9

--- src/main/java/edu/mum/cs544/PostService/contrtollers/PostContrtoller.java
+++ src/main/java/edu/mum/cs544/PostService/contrtollers/PostContrtoller.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,13 +50,13 @@ public class PostContrtoller {
         return new ResponseDto<Post>("Post added", false, post , null);        
     }
 
-    @PostMapping(value = "/update/{id}")
+    @PutMapping(value = "/update/{id}")
     public ResponseDto<Post> update(@PathVariable int id, @RequestBody Post post) {
         postService.update(post,id);                
         return new ResponseDto<Post>("Post was updated", false, post, null);        
     }
 
-    @GetMapping(value = "/delete/{id}")
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseDto<String> delete(@PathVariable int id) {
         postService.delete(id);
         return new ResponseDto<String>("Post was deleted", false, null , null);
